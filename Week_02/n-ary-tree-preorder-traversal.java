bf537588a6fbfe8dd37accde826ffdc28966eaa6
gaopengpy <67363230+gaopengpy@users.noreply.github.com>
2020-06-28 15:13:47 +0800

    Create n-ary-tree-preorder-traversal.java

---
+++ Week_02/n-ary-tree-preorder-traversal.java
@@ -0,0 +1,19 @@
+
+class Solution {
+
+	List<Integer> list = new ArrayList();
+
+	public List<Integer> preorder(Node root) {
+		if (root == null) {
+			return list;
+		} else {
+			list.add(root.val);
+			for (Node node : root.children) {
+				preorder(node);
+			}
+            
+            return list;
+		}
+	}
+
+}
