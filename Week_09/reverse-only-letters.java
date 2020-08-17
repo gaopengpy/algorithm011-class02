6441db0ea58c609888ad1bcd8e4f5e60668c76d7
gaopeng <gaopeng@ihanchen.com>
2020-08-17 18:00:37 +0800

    09

---
+++ Week_09/reverse-only-letters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String reverseOnlyLetters(String S) {
+        Stack<Character> letters = new Stack<>();
+        for (char c : S.toCharArray()) {
+            if (Character.isLetter(c)) {
+                letters.push(c);
+            }
+        }
+
+        StringBuffer ans = new StringBuffer();
+        for (char c : S.toCharArray()) {
+            if (Character.isLetter(c)) {
+                ans.append(letters.pop());
+            } else {
+                ans.append(c);
+            }
+        }
+
+        return ans.toString();
+    }
+}
